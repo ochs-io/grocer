d26e2f707a66e98ae87281aec97b58f1f99c96bf
Marc Ochsner <marc.ochsner@woundzoom.com>
2022-08-06 15:28:58 -0400

    Complete models and Django Model API from shell

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-from django.db import models
+import datetime
 
+from django.db import models
+from django.utils import timezone
 
 """
 Here, each model is represented by a class that subclasses django.db.models.Model.
@@ -19,10 +21,19 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published') # if not given human readable name like so, would use machine variable name
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
 
 
