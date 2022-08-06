9b9b7a96e1be87e56706d6736154d1513fddd484
Marc Ochsner <marc.ochsner@woundzoom.com>
2022-08-06 15:34:56 -0400

    Allow edit of Questions in admin settings

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+
+# Make poll app modifiable in admin
+from django.contrib import admin
+from .models import Question
+
+admin.site.register(Question)
+
 # Register your models here.
