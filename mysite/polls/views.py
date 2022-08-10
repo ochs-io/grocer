37410e51d5ab16165ee7dcd1e8a932bca8016a99
Marc Ochsner <marc.ochsner@woundzoom.com>
2022-08-10 19:43:10 -0400

    tutorial03 - removing hardcode URL names

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -13,19 +13,19 @@ def index(request):
     }
     return HttpResponse(template.render(context, request))
 
-    return render(request, 'polls/index.html', context) # same as above but shorter
+    # return render(request, 'polls/index.html', context) # same as above but shorter
 
     #output = ', '.join([q.question_text for q in latest_question_list])
     #return HttpResponse(output)
 
 def detail(request, question_id):
-    #try:
-    #    question = Question.objects.get(pk=question_id)
-    #except Question.DoesNotExist:
-    #    raise Http404("Question does not exist")
-    #return HttpResponse("You're looking at question %s." % question_id)
-    # Same as:
     question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+def detail_manual(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
